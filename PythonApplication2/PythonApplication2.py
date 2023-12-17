c4f16ba6c737aa902272dc2d23168db941ab6710
Lokalus <Lokalus@IT10R103-101>
2023-12-17 11:35:14 +0200

    Add project files.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,185 @@
+
+#height = int(input("Height?"))
+
+
+#if height < 140:
+    #print("You need to pay, 10 bucks")
+#else:
+    #print("You need to pay, 12 bucks")
+
+
+
+
+
+#grade = int(input("Grade? :"))
+
+#if grade >= 90:
+    #print("You got A grade!")
+#elif grade >= 80:
+    #print("You got B")
+#elif grade >= 70:
+    #print("You got C!")
+#elif grade >= 60:
+    #print("You got D!")
+#elif grade >= 50:
+    #print("You got F!")
+    
+
+
+
+#age = int(input("Age?: "))
+#if age >= 13:
+    #print("You are a teenager!")
+#elif age <= 18:
+    #print("You are not a teenager")
+
+
+#password = int(input("Password length: :"))
+#if password <= 8:
+    #print("very weak password!!11!")
+#elif  9 <= password <= 12:
+    #print("medium strength!!")
+#elif password > 12:
+    #print("very stronk password!!!1!")
+
+
+#num1 = float(input("Enter num1: "))
+#num2 = float(input("enter num2: "))
+
+#if num1 > num2:
+    #print("num1 is greater")
+#elif num1 == num2:
+    #print("num1 and num2 are equal")
+#else:
+    #print("num2 is greater")
+
+
+#time = input("Enter time in 24 hour format:")
+
+#if time < "12:00":
+    #print("Good morning sir!")
+#elif "12:00" <= time <= "18:00":
+    #print("Good afternoon, sir!")
+#else:
+    #print("Good evening, sir!")
+
+
+
+
+
+
+#fruits = [1, 2, 3, 4, 5]
+
+
+
+#for fruit in fruits:
+    #print(fruit)
+    
+
+
+
+
+#income = int(input("Monthly income? :"))
+#age = int(input("Your age?:"))
+
+
+#if income <= 1000:
+    #print("you rich boy")
+#elif 200 <= income <= 500:
+    #print("You broke boi")
+#else:
+    #print("You stable boi")
+    
+#if age > 10:
+    #print("You old boi")
+#elif 10 <= age <= 17:
+    #print("You young boi")
+#else:
+    #print("you very old")
+    
+#if age > 18 and income > 800:
+    #print("You can adopt a pet")
+#else:
+    #print("You cant adopt a pet")
+    
+
+##
+
+
+
+#age = int(input("Hey, age? :"))
+
+
+#if age < 13:
+    #print("You need to pay 8 bucks")
+#elif 13 <= age <= 17:
+    #print("You need to pay 10 bucks")
+#elif 18 <= age:
+    #print("You need to pay 12 bucks")
+    
+
+#final_grade = int(input("Grade? :"))
+
+#if final_grade >= 90:
+    #print("You got A!")
+#elif final_grade >= 80:
+    #print("You got B")
+#elif final_grade >= 70:
+    #print("You got C!")
+#elif final_grade >= 60:
+    #print("You got D!")
+#elif final_grade >= 50:
+    #print("You Failed")
+
+
+
+
+
+
+
+#sport_team_appy = int(input("What is your age?"))
+#sport_team_experience = input("Any Experience")
+
+#if sport_team_experience == "yes":
+    #print("good")
+#else:
+    #print("Bad")
+    
+
+
+#if 12 <= sport_team_appy <= 16:
+    #print("You can join The Team!")
+#else:
+    #print("No, You can't join the team")
+
+#weight = float(input("Weight?"))
+#height = float(input("Height?"))
+
+#bmi = weight / (height/weight)**2
+
+
+#print(f"Your bmi is {bmi}")
+
+#if bmi <= 18.4:
+    #print("You are underweight.")
+#elif bmi <= 24.9:
+    #print("You are healthy.")
+#elif bmi <= 29.9:
+    #print("You are over weight.")
+#elif bmi <= 34.9:
+    #print("You are severely over weight.")
+#elif bmi <= 39.9:
+    #print("You are obese.")
+#else:
+    #print("You are severely obese.")
+    
+
+
+
+#temp = int(input("Temperatura?"))
+
+#if temp > 30:
+    #print("karsta diena")
+#else:
+    #print("nekarsta diena!")
+
